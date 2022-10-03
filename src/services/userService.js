9f235c9b1e79c890bcfb51829222c6f31dad9600
NguyenMinhThien263 <nguyenminhthien263@gmail.com>
2022-10-03 20:06:53 +0700

    render table user with data

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,8 @@
 import axios from '../axios';
 const handleLoginApi = (email, password) => {
-    return axios.post('/api/login', {email, password})
+    return axios.post('/api/login', { email, password })
 }
-export { handleLoginApi };
+const getAllUsers = (inputId) => {
+    return axios.get(`/api/get-all-user?id=${inputId}`)
+}
+export { handleLoginApi, getAllUsers };
