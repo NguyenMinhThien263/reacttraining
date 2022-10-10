5eb9c2bf8cc1f6d31b9cef900c46831a9cbecab7
NguyenMinhThien263 <nguyenminhthien263@gmail.com>
2022-10-10 16:36:55 +0700

    add section medical-facility, outstanding-doctor, handbook

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,44 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './Specialty.scss';
 import Slider from "react-slick";
-
-import specialtyImg from '../../../assets/specialty/co-xuong-khop.jpg'
-
-class Specialty extends Component {
+class HandBook extends Component {
 
     render() {
         return (
-            <div className="section-share specialty">
+            <div className="section-share handbook">
                 <div className="section-container">
                     <div className="section-header">
-                        <span className="title-section">Chuyên khoa phổ biến</span>
+                        <span className="title-section">Cẩm Nang</span>
                         <button className="btn-section">Xem thêm</button>
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
                             <div className="section-customize">
-                                <div className="section-img specialty-img" />
+                                <div className="section-img handbook-img" />
                                 <div className="">Cơ xương khớp 1</div>
                             </div>
                             <div className="section-customize">
-                                <div className="section-img specialty-img" />
+                                <div className="section-img handbook-img" />
                                 <div className="">Cơ xương khớp 2</div>
                             </div>
                             <div className="section-customize">
-                                <div className="section-img specialty-img" />
+                                <div className="section-img handbook-img" />
                                 <div className="">Cơ xương khớp 3</div>
                             </div>
                             <div className="section-customize">
-                                <div className="section-img specialty-img" />
+                                <div className="section-img handbook-img" />
                                 <div className="">Cơ xương khớp 4</div>
                             </div>
                             <div className="section-customize">
-                                <div className="section-img specialty-img" />
+                                <div className="section-img handbook-img" />
                                 <div className="">Cơ xương khớp 5</div>
                             </div>
                             <div className="section-customize">
-                                <div className="section-img specialty-img" />
+                                <div className="section-img handbook-img" />
                                 <div className="">Cơ xương khớp 6</div>
                             </div>
                         </Slider>
@@ -61,4 +57,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
